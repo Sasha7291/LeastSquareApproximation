6b9b51f32a5e99ebb53c2daea226970439c5cf3f
Terentev A.D. <terentev.ad18@physics.msu.ru>
2025-05-26 02:01:58 +0300

    Add 3d approximation

--- include/lsa_approximator.h
+++ include/lsa_approximator.h
@@ -1,12 +1,6 @@
 #pragma once
 
 #include "lsa_solver.hpp"
-#include "psr_average.h"
-#include "psr_polynomial.h"
-#include "psr_standartize.h"
-#include "psr_variance.h"
-
-#include <numeric>
 
 
 namespace lsa
@@ -26,13 +20,23 @@ public:
 
     [[nodiscard]] Result linear(Keys x, Values y) const;
     [[nodiscard]] Result linear(Keys x, Keys y, Values z) const;
-    [[nodiscard]] Result polynomial(Keys x, Values y, const std::size_t N) const;
+    [[nodiscard]] Result polynomial(Keys x, Values y, std::size_t N) const;
+    [[nodiscard]] Result polynomial(Keys x, Keys y, Values z, std::size_t N) const;
 
 private:
-    [[nodiscard]] constexpr unsigned binomialCoefficient(unsigned n, unsigned k) const;
+    [[nodiscard]] constexpr std::size_t binomialCoefficient(std::size_t n, std::size_t k) const;
     [[nodiscard]] Coefficients coefficientReverseStandardization(Coefficients coeffs, Type average, Type variance) const;
-    [[nodiscard]] Coefficients coefficientReverseStandardization(Coefficients coeffs, Keys data) const;
-    [[nodiscard]] constexpr unsigned factorial(unsigned n) const;
+    [[nodiscard]] Coefficients coefficientReverseStandardization(
+        Coefficients coeffs,
+        Type averageX,
+        Type varianceX,
+        Type averageY,
+        Type varianceY,
+        Type averageZ,
+        Type varianceZ
+    ) const;
+    [[nodiscard]] constexpr double monomial(std::size_t n, double x, double y) const;
+    [[nodiscard]] std::pair<std::size_t, std::size_t> degreeToIndicies(std::size_t n) const;
 
 };
 
--- src/lsa_approximator.cpp
+++ src/lsa_approximator.cpp
@@ -1,5 +1,11 @@
 #include "lsa_approximator.h"
 
+#include "psr_average.h"
+#include "psr_standartize.h"
+#include "psr_variance.h"
+
+#include <numeric>
+
 
 namespace lsa
 {
@@ -18,43 +24,51 @@ Result Approximator::linear(Keys x, Values y) const
 
 Result Approximator::linear(Keys x, Keys y, Values z) const
 {
-    auto averX = psr::Average<double>{}(x);
-    auto averY = psr::Average<double>{}(y);
-    auto averZ = psr::Average<double>{}(z);
-    auto varX = psr::Variance<double>{averX}(x);
-    auto varY = psr::Variance<double>{averY}(y);
-    auto varZ = psr::Variance<double>{averZ}(z);
-    auto tempX = psr::Standartize<double>{averX, varX}(x);
-    auto tempY = psr::Standartize<double>{averY, varY}(y);
-    auto tempZ = psr::Standartize<double>{averZ, varZ}(z);
+    // auto averX = psr::Average<double>{}(x);
+    // auto averY = psr::Average<double>{}(y);
+    // auto averZ = psr::Average<double>{}(z);
+    // auto varX = psr::Variance<double>{averX}(x);
+    // auto varY = psr::Variance<double>{averY}(y);
+    // auto varZ = psr::Variance<double>{averZ}(z);
+    // auto tempX = psr::Standartize<double>{averX, varX}(x);
+    // auto tempY = psr::Standartize<double>{averY, varY}(y);
+    // auto tempZ = psr::Standartize<double>{averZ, varZ}(z);
 
-    dynamic_matrix::SquareMatrix<Type> A(2);
-    dynamic_matrix::Matrix<Type> B(2, 1);
+    // dynamic_matrix::SquareMatrix<Type> A(2);
+    // dynamic_matrix::Matrix<Type> B(2, 1);
 
-    std::function<double(Keys, Keys)> crossedSum = [](Keys keys1, Keys keys2) -> double {
-        return std::transform_reduce(keys1.cbegin(), keys1.cend(), keys2.cbegin(), static_cast<Type>(0), std::plus<>(), [](Type value1, Type value2) -> Type {
-            return value1 * value2;
-        });
-    };
+    // std::function<double(Keys, Keys)> crossedSum = [](Keys keys1, Keys keys2) -> double {
+    //     return std::transform_reduce(keys1.cbegin(), keys1.cend(), keys2.cbegin(), static_cast<Type>(0), std::plus<>(), [](Type value1, Type value2) -> Type {
+    //         return value1 * value2;
+    //     });
+    // };
 
-    A(0, 0) = crossedSum(tempX, tempX);
-    A(0, 1) = A(1, 0) = crossedSum(tempX, tempY);
-    A(1, 1) = crossedSum(tempY, tempY);
-    B(0, 0) = crossedSum(tempX, tempZ);
-    B(0, 0) = crossedSum(tempY, tempZ);
+    // A(0, 0) = crossedSum(tempX, tempX);
+    // A(0, 1) = A(1, 0) = crossedSum(tempX, tempY);
+    // A(1, 1) = crossedSum(tempY, tempY);
+    // B(0, 0) = crossedSum(tempX, tempZ);
+    // B(0, 0) = crossedSum(tempY, tempZ);
 
-    auto unknownColumn = Solver()(A, B);
-    if (!unknownColumn.has_value())
-        throw Exception("Coefficients matrix is irreversible");
+    // auto unknownColumn = Solver()(A, B);
+    // if (!unknownColumn.has_value())
+    //     throw Exception("Coefficients matrix is irreversible");
 
-    return {
-        averZ - unknownColumn.value()(0, 0) * varZ * averX / varX - unknownColumn.value()(1, 0) * varZ * averY / varY,
-        unknownColumn.value()(0, 0) * varZ / varX,
-        unknownColumn.value()(1, 0) * varZ / varY
-    };
+    // return {
+    //     averZ - unknownColumn.value()(0, 0) * varZ * averX / varX - unknownColumn.value()(1, 0) * varZ * averY / varY,
+    //     unknownColumn.value()(0, 0) * varZ / varX,
+    //     unknownColumn.value()(1, 0) * varZ / varY
+    // };
+    try
+    {
+        return polynomial(x, y, z, 2);
+    }
+    catch (const Exception &)
+    {
+        throw;
+    }
 }
 
-Result Approximator::polynomial(Keys x, Values y, const std::size_t N) const
+Result Approximator::polynomial(Keys x, Values y, std::size_t N) const
 {
     auto aver = psr::Average<double>{}(x);
     auto var = psr::Variance<double>{aver}(x);
@@ -69,9 +83,9 @@ Result Approximator::polynomial(Keys x, Values y, const std::size_t N) const
             return total + std::pow(value, i);
         });
 
-    for (auto i = 0ull; i < N; ++i)
+    for (std::size_t i = 0ull; i < N; ++i)
     {
-        for (auto j = i; j < N; ++j)
+        for (std::size_t j = i; j < N; ++j)
             A(i, j) = A(j, i) = sums[i + j];
 
         B(i, 0) = std::transform_reduce(tempX.cbegin(), tempX.cend(), y.cbegin(), static_cast<Type>(0), std::plus<>(), [i](Type value1, Type value2) -> Type {
@@ -86,14 +100,45 @@ Result Approximator::polynomial(Keys x, Values y, const std::size_t N) const
     return coefficientReverseStandardization(unknownColumn.value().column(0), aver, var);
 }
 
-constexpr unsigned Approximator::binomialCoefficient(unsigned n, unsigned k) const
+Result Approximator::polynomial(Keys x, Keys y, Values z, std::size_t N) const
 {
-    return factorial(n) / (factorial(k) * factorial(n - k));
+    auto averX = psr::Average<double>{}(x);
+    auto varX = psr::Variance<double>{averX}(x);
+    auto tempX = psr::Standartize<double>{averX, varX}(x);
+    auto averY = psr::Average<double>{}(y);
+    auto varY = psr::Variance<double>{averY}(y);
+    auto tempY = psr::Standartize<double>{averY, varY}(y);
+    auto averZ = psr::Average<double>{}(z);
+    auto varZ = psr::Variance<double>{averZ}(z);
+    auto tempZ = psr::Standartize<double>{averZ, varZ}(z);
+
+    N = N * (N + 1) / 2;
+    dynamic_matrix::SquareMatrix<double> A{N};
+    dynamic_matrix::Matrix<double> B{N, 1};
+
+    for (std::size_t i = 0ull; i < N; ++i)
+    {
+        for (std::size_t j = 0ull; j < N; ++j)
+            A(i, j) = A(j, i) = std::transform_reduce(tempX.cbegin(), tempX.cend(), tempY.cbegin(), 0.0, std::plus<>(), [this, i, j](double value1, double value2) -> double {
+                return monomial(i, value1, value2) * monomial(j, value1, value2);
+            });
+
+        double sum = 0.0;
+        for (std::size_t k = 0ull; k < N; ++k)
+            sum += tempZ[k] + monomial(i, tempX[k], tempY[k]);
+        B(i, 0) = sum;
+    }
+
+    auto unknownColumn = Solver()(A, B);
+    if (!unknownColumn.has_value())
+        throw Exception("Coefficients matrix is irreversible");
+
+    return coefficientReverseStandardization(unknownColumn.value().column(0), averX, varX, averY, varY, averZ, varZ);
 }
 
-constexpr unsigned Approximator::factorial(unsigned n) const
+constexpr std::size_t Approximator::binomialCoefficient(std::size_t n, std::size_t k) const
 {
-    return (n > 1) ? n * factorial(n - 1) : 1;
+    return std::tgamma(n + 1) / (std::tgamma(k + 1) * std::tgamma(n - k + 1));
 }
 
 Coefficients Approximator::coefficientReverseStandardization(Coefficients coeffs, Type average, Type variance) const
@@ -108,10 +153,51 @@ Coefficients Approximator::coefficientReverseStandardization(Coefficients coeffs
     return result;
 }
 
-Coefficients Approximator::coefficientReverseStandardization(Coefficients coeffs, Keys data) const
+Coefficients Approximator::coefficientReverseStandardization(
+    Coefficients coeffs,
+    Type averageX,
+    Type varianceX,
+    Type averageY,
+    Type varianceY,
+    Type averageZ,
+    Type varianceZ
+) const
 {
-    auto aver = psr::Average<double>{}(data);
-    return coefficientReverseStandardization(coeffs, aver, psr::Variance<double>{aver}(data));
+    Coefficients result(coeffs.size(), 0.0);
+
+    for (size_t k = 0; k < coeffs.size(); ++k)
+    {
+        const auto [p, q] = degreeToIndicies(k);
+        double factor = varianceZ / (std::pow(varianceX, p) * std::pow(varianceY, q));
+
+        for (std::size_t i = 0; i <= p; ++i)
+            for (std::size_t j = 0; j <= q; ++j)
+                result[(i + j) * (i + j + 1) / 2 + i] +=
+                    coeffs[k] * factor
+                    * binomialCoefficient(p, i) * binomialCoefficient(q, j)
+                    * pow(-averageX, p - i) * pow(-averageY, q - j);
+    }
+
+    result[0] += averageZ;
+    return result;
+}
+
+constexpr double Approximator::monomial(std::size_t n, double x, double y) const
+{
+    const auto [p, q] = degreeToIndicies(n);
+    return std::pow(x, p) * std::pow(y, q);
+}
+
+std::pair<std::size_t, std::size_t> Approximator::degreeToIndicies(std::size_t n) const
+{
+    std::size_t m = 0ull;
+    while ((m + 1) * (m + 2) / 2 <= n)
+        ++m;
+
+    std::size_t p = n - m * (m + 1) / 2 - 1;
+    std::size_t q = m - p;
+
+    return std::make_pair(p, q);
 }
 
 }
